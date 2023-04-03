a3cbaf1365c1038dcdddea4761690216857b6fa3
KermitTheStarlord <113591565+KermitTheStarlord@users.noreply.github.com>
2023-04-03 04:10:36 +0200

    Add files via upload

--- Power Laws.R
+++ Power Laws.R
@@ -1,5 +1,6 @@
 library(poweRlaw)
 
+# Name of work
 play <- data.frame(table(rawData$Name.of.Work))
 colnames(play) <- c("play","Freq")
 
@@ -8,6 +9,24 @@ est <- estimate_xmin(data_pl)
 data_pl$xmin <- est$xmin
 data_pl$pars <- est$pars
 bs <- bootstrap_p(data_pl)
+
+# Log-Normal
+data_alt <- dislnorm$new(play$Freq)
+data_alt$xmin <- est$xmin
+data_alt$pars <- estimate_pars(data_alt)
+comp <- compare_distributions(data_pl, data_alt)
+
+# Exponential
+data_alt <- disexp$new(play$Freq)
+data_alt$xmin <- est$xmin
+data_alt$pars <- estimate_pars(data_alt)
+comp <- compare_distributions(data_pl, data_alt)
+
+# Poisson
+data_alt <- dispois$new(play$Freq)
+data_alt$xmin <- est$xmin
+data_alt$pars <- estimate_pars(data_alt)
+comp <- compare_distributions(data_pl, data_alt)
 
 nationalityOfComposer <- data.frame(table(rawData$Nationality.of.Composer))
 colnames(natioanlityOfComposer) <- c("nat","Freq")
